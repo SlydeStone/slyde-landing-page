fb9dcb680fc712142e54b32ee9277ad9ae90c0c9
dpeza <48867902+dpeza@users.noreply.github.com>
2025-09-01 14:06:21 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // important for static export if you use <Image>
+  },
 };
 
 export default nextConfig;
